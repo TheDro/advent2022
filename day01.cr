46927ef893653f8225a6e104c3e6c17f0e117638
TheDro <andrewsc32@protonmail.com>
2022-11-28 12:54:48 -0500

    Basic imagesc

--- day01.cr
+++ day01.cr
@@ -1,1 +1,26 @@
 puts "hello world"
+require "stumpy_png"
+include StumpyPNG
+
+
+def imagesc(img)
+  maxVal = 1.0
+  nx = img.size
+  ny = img[0].size
+  canvas = Canvas.new(nx,ny)
+  (0..nx-1).each do |i|
+    (0..ny-1).each do |j|
+      value = (img[i][j]/maxVal*255).to_i
+      canvas[i,j] = RGBA.from_rgb(value,value/2,value/4)
+    end
+  end
+  StumpyPNG.write(canvas, "output1.png")
+  canvas
+end
+
+img = [[0.1,0.8,0.1,0.9]]
+imagesc(img)
+
+# debugger
+
+puts "done"
