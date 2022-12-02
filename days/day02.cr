69e039d1ca1c71fb92724141c6a4d268c2e54323
TheDro <andrewsc32@protonmail.com>
2022-12-02 12:20:57 -0500

    Completed day 2

--- days/day02.cr
+++ days/day02.cr
@@ -1,15 +1,35 @@
 require "../common.cr"
 
-content = File.read("./days/day01.data")
+content = File.read("./days/day02.data")
 
-calories = parse_data(content, "\n\n", "\n")
+rounds = parse_data(content, "\n", " ")
 
-totals = calories.map do |group|
-  # next 0 if group.is_a?(String)
-  group.map(&.to_i).sum
+scorecard = {
+  "A" => {"X" => 1+3, "Y" => 2+6, "Z" => 3+0}, 
+  "B" => {"X" => 1+0, "Y" => 2+3, "Z" => 3+6}, 
+  "C" => {"X" => 1+6, "Y" => 2+0, "Z" => 3+3}, 
+}
+
+
+total = 0
+rounds.each do |round|
+  total += scorecard[round[0]][round[1]]
 end
 
-top3 = totals.sort.reverse.first(3)
+
+scorecard2 = {
+  "A" => {"X" => 3+0, "Y" => 1+3, "Z" => 2+6}, 
+  "B" => {"X" => 1+0, "Y" => 2+3, "Z" => 3+6}, 
+  "C" => {"X" => 2+0, "Y" => 3+3, "Z" => 1+6}, 
+}
+
+total2 = 0
+rounds.each do |round|
+  total2 += scorecard2[round[0]][round[1]]
+end
+
+puts total
+puts total2
 
 debugger
 
