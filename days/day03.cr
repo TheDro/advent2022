3d72cb4d0d629fc68a8716c748ac4d74f4874c71
TheDro <andrewsc32@protonmail.com>
2022-12-03 06:15:13 -0500

    Completed part 1

--- days/day03.cr
+++ days/day03.cr
@@ -1,35 +1,35 @@
 require "../common.cr"
 
-content = File.read("./days/day02.data")
+content = File.read("./days/day03.data")
 
-rounds = parse_data(content, "\n", " ")
+bags = parse_data(content, "\n", "")
 
-scorecard = {
-  "A" => {"X" => 1+3, "Y" => 2+6, "Z" => 3+0}, 
-  "B" => {"X" => 1+0, "Y" => 2+3, "Z" => 3+6}, 
-  "C" => {"X" => 1+6, "Y" => 2+0, "Z" => 3+3}, 
-}
+def part1(bags)
+  doubles = [] of Char
 
+  bags.each do |left|
+    right = left.pop(left.size//2)
+    doubles << (right&left)[0][0]
+  end
 
-total = 0
-rounds.each do |round|
-  total += scorecard[round[0]][round[1]]
+  sum = 0
+  doubles.each do |char|
+    value = char.ord <= 'Z'.ord ? char.ord - 'A'.ord + 27 : char.ord - 'a'.ord + 1
+    sum += value
+  end
+
+  puts "part 1: #{sum}"
+  sum
 end
 
 
-scorecard2 = {
-  "A" => {"X" => 3+0, "Y" => 1+3, "Z" => 2+6}, 
-  "B" => {"X" => 1+0, "Y" => 2+3, "Z" => 3+6}, 
-  "C" => {"X" => 2+0, "Y" => 3+3, "Z" => 1+6}, 
-}
 
-total2 = 0
-rounds.each do |round|
-  total2 += scorecard2[round[0]][round[1]]
-end
 
-puts total
-puts total2
+
+
+
+
+part1(bags)
 
 debugger
 
