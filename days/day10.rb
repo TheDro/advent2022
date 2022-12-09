9f5cdd6d066f2b9fc47e2716bda8bae31cc52b91
TheDro <andrewsc32@protonmail.com>
2022-12-09 12:40:46 -0500

    Updated correct filename

--- days/day10.rb
+++ days/day10.rb
@@ -1,5 +1,5 @@
 require 'pry'
 load 'common.rb'
-content = File.read("./days/day09.data")
-# content = File.read("./days/day09.test.data")
+content = File.read("./days/day10.data")
+# content = File.read("./days/day10.test.data")
 $moves = parse_data(content,"\n"," ")
