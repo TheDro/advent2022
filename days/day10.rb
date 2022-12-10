1cb9a31dc721fbea32f22f70732e84e7c9bfc1e7
TheDro <andrewsc32@protonmail.com>
2022-12-10 07:11:16 -0500

    Completed day 10

--- days/day10.rb
+++ days/day10.rb
@@ -1,34 +1,28 @@
 require 'pry'
 load 'common.rb'
-# content = File.read("./days/day10.data")
-content = File.read("./days/day10.test.data")
+content = File.read("./days/day10.data")
+# content = File.read("./days/day10.test.data")
 $instructions = parse_data(content,"\n")
 
 # remove from beginning: shift
 # add to end: push
 
 def generate_signals(instructions = $instructions)
-  $signals = Array.new(instructions.length+2){0}
+  $signals = []
   commands = []
   cursor = 1
   instructions.each_index do |i|
     instruction = instructions[i]
-    
     if /noop/.match(instruction)
       commands.push(0)
     elsif value = /addx ([-0-9]+)/.match(instruction)
       commands.push(0)
       commands.push(value[1].to_i)
     end
-    $signals[i] = cursor
-    if current_command = commands.shift
-      cursor = cursor + current_command
-    end
-    
   end
   while !commands.empty?
-    cursor = cursor + commands.shift
     $signals.push(cursor)
+    cursor = cursor + commands.shift
   end
 
   $signals
@@ -46,6 +40,23 @@ def part1
   result
 end
 
+def part2
+  signals = generate_signals
+  image = Array.new(10){ Array.new(40){" "} }
+  signals.each_index do |i|
+    x = i%40
+    if (x-signals[i]).abs <= 1
+      image[i/40][x] = '#'
+    end
+  end
+  image.each do |row|
+    puts row.join
+  end
+
+end
+
+
 generate_signals($instructions)
 pp (1..$signals.length).zip($signals)
 pp part1
+part2
